489d5239936dcd7fb3d38384fbed706f41045bb7
Caio <caio@teste.com>
2026-04-17 19:30:02 -0300

    Adding the home route

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,11 +1,15 @@
 from django.views import generic
+from django.http import HttpResponse
 from .models import Post
 
 class PostView(generic.ListView):
     queryset = Post.objects.filter(status='published').order_by('-created')
-    template_name = 'index.html' 
+    template_name = 'index.html'
     context_object_name = 'post_list'
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post-detail.html'
+
+def hello_world(request):
+    return HttpResponse("hello world")
