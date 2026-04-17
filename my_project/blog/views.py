8b1060f258d211d5276550777b57601e56bd7618
Caio <caio@teste.com>
2026-04-17 19:47:45 -0300

    Correcting missing files

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
 from django.http import HttpResponse
+from .models import Post
 
-def post_view(request):
-    return HttpResponse("Hello World")
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status='published').order_by('-created')
+    template_name = 'index.html'
+    context_object_name = 'post_list'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post-detail.html'
+
+def hello_world(request):
+    return HttpResponse("hello world")
